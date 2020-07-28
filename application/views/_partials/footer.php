005952129b5bb5e00acf16d39f2690b14c77c254
adprm <aditiyaprmn00@gmail.com>
2020-07-28 10:42:45 +0700

    footer component

---
+++ application/views/_partials/footer.php
@@ -0,0 +1,12 @@
+<footer class="py-5">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; <?php echo WEB_NAME ." ". Date('Y') ?></p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<style>
+footer {
+  background-color: #300030;
+}
+</style>
