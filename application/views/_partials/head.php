11a31013807bcff61e66f81767f354e4e71b4871
adprm <aditiyaprmn00@gmail.com>
2020-07-28 10:44:32 +0700

    header component

---
+++ application/views/_partials/head.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title><?php echo WEB_NAME ." ". ucfirst($this->uri->segment(1)) ." ". ucfirst($this->uri->segment(2)) ?></title>
+
+<!-- Bootstrap core CSS -->
+<link href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="<?php echo base_url('css/business-frontpage.css') ?>" rel="stylesheet">
