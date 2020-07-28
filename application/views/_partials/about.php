40abde4f8471a5e68d05a9932af0d607f455328c
adprm <aditiyaprmn00@gmail.com>
2020-07-28 10:41:58 +0700

    about page

---
+++ application/views/_partials/about.php
@@ -0,0 +1,14 @@
+<h2><?php echo $about->about_title ?></h2>
+<hr>
+<p><?php echo $about->about_desk ?></p>
+<a class="btn btn-dark" href="<?php echo site_url('about') ?>">Continued &raquo;</a>
+
+<style>
+h2 {
+    color: #480048;
+}
+
+.btn {
+    background: #300030;
+}
+</style>
